6893928f156ba4d9c48867b2b1ffb168d49b4764
aaron-boyd <boydaaron06@gmail.com>
2018-10-27 18:08:07 -0400

    at btstack

--- main/main.cpp
+++ main/main.cpp
@@ -65,20 +65,20 @@ void app_main() {
 	oled = OLED(GPIO_NUM_21, GPIO_NUM_22, SSD1306_128x64);
 	if (oled.init()) {
 		ESP_LOGI("OLED", "oled inited");
-//		oled.draw_rectangle(10, 30, 20, 20, WHITE);
-//		oled.select_font(0);
-//		//deprecated conversion from string constant to 'char*'
-//		oled.draw_string(0, 0, "glcd_5x7_font_info", WHITE, BLACK);
-//		ESP_LOGI("OLED", "String length:%d",
-//				oled.measure_string("glcd_5x7_font_info"));
-//		oled.select_font(1);
-//		oled.draw_string(0, 18, "tahoma_8pt_font_info", WHITE, BLACK);
-//		ESP_LOGI("OLED", "String length:%d",
-//				oled.measure_string("tahoma_8pt_font_info"));
-//		oled.draw_string(55, 30, "Hello ESP32!", WHITE, BLACK);
-//		oled.refresh(true);
-//		vTaskDelay(3000 / portTICK_PERIOD_MS);
-		xTaskCreatePinnedToCore(&myTask, "adctask", 2048, NULL, 5, NULL, 1);
+		oled.draw_rectangle(10, 30, 20, 20, WHITE);
+		oled.select_font(0);
+		//deprecated conversion from string constant to 'char*'
+		oled.draw_string(0, 0, "glcd_5x7_font_info", WHITE, BLACK);
+		ESP_LOGI("OLED", "String length:%d",
+				oled.measure_string("glcd_5x7_font_info"));
+		oled.select_font(1);
+		oled.draw_string(0, 18, "tahoma_8pt_font_info", WHITE, BLACK);
+		ESP_LOGI("OLED", "String length:%d",
+				oled.measure_string("tahoma_8pt_font_info"));
+		oled.draw_string(55, 30, "Hello ESP32!", WHITE, BLACK);
+		oled.refresh(true);
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
+		//xTaskCreatePinnedToCore(&myTask, "adctask", 2048, NULL, 5, NULL, 1);
 	} else {
 		ESP_LOGE("OLED", "oled init failed");
 	}
